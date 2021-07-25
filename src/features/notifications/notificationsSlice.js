09ce2aa0f1eb94c824e8eac98bb1badb43f6db40
shirasu-aq <risako.shirasu@asia-quest.jp>
2021-07-26 00:31:33 +0900

    新しい通知を表示させる

--- src/features/notifications/notificationsSlice.js
+++ src/features/notifications/notificationsSlice.js
@@ -18,9 +18,19 @@ export const fetchNotifications = createAsyncThunk(
 const notificationsSlice = createSlice({
   name: "notifications",
   initialState: [],
-  reducers: {},
+  reducers: {
+    allNotificationsRead(state, action) {
+      state.forEach((notification) => {
+        notification.read = true;
+      });
+    },
+  },
   extraReducers: {
     [fetchNotifications.fulfilled]: (state, action) => {
+      state.forEach((notification) => {
+        // Any notifications we've read are no longer new
+        notification.isNew = !notification.read;
+      });
       state.push(...action.payload);
       // Sort with newest first
       state.sort((a, b) => b.date.localeCompare(a.date));
@@ -28,6 +38,7 @@ const notificationsSlice = createSlice({
   },
 });
 
-export default notificationsSlice.reducer;
+export const { allNotificationsRead } = notificationsSlice.actions;
 
+export default notificationsSlice.reducer;
 export const selectAllNotifications = (state) => state.notifications;
